e549b8fc838c1ed0c89dc3aab5e79215647f7f21
Lucas Silva <lucassilva996@outlook.com>
2021-10-20 00:04:34 -0300

    FrontEnd app concluido, realizando as operações de CRUD

--- PaymentAPI/Startup.cs
+++ PaymentAPI/Startup.cs
@@ -30,6 +30,9 @@ namespace PaymentAPI
             services.AddDbContext<PaymentDetailContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddControllers();
+            services.AddCors();
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentAPI", Version = "v1" });
@@ -39,6 +42,11 @@ namespace PaymentAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
